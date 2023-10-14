25f33ca2a439068432ffa8c8ca668650f1e950e9
khalilelamraoui <khalilelam16@gmail.com>
2023-10-14 08:54:08 -0700

    fixed problems

--- printf.c
+++ printf.c
@@ -16,6 +16,10 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (*format)
 	{
 		if (*format != '%')
