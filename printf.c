6bbd25178ed5870e9fb19296c44d9cba8fcb2a34
khalilelamraoui <khalilelam16@gmail.com>
2023-10-14 06:59:39 -0700

    handling %c %s %%

---
+++ printf.c
@@ -0,0 +1,54 @@
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _printf - Print formatted output to stdout
+ * @format: The format string
+ * @...: Variable number of arguments
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int char_count = 0, c, len;
+	const char *str;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_count++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				char_count++;
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, const char *);
+				len = 0;
+				while (str[len] != '\0')
+					len++;
+				write(1, str, len);
+				char_count += len;
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				char_count++;
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (char_count);
+}
