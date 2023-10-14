347ad07f8fb2485df4a84c8e03e57237baa4f136
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-14 08:31:35 -0700

    dond

--- printf.c
+++ printf.c
@@ -1,5 +1,41 @@
 #include"main.h"
 /**
+ * int_to_binary - Convert an unsigned integer to a binary string
+ * @buffer: The buffer to store the binary string
+ * @num: The unsigned integer
+ *
+ * Return: The number of characters written to the buffer, or -1 on error
+ */
+int int_to_binary(char *buffer, unsigned int num)
+{
+	int count = 0, i = 0, j;
+	char temp;
+
+	if (buffer == NULL)
+		return (-1);
+	if (num == 0)
+	{
+		buffer[0] = '0';
+		buffer[1] = '\0';
+		return (1);
+	}
+	while (num > 0)
+	{
+		buffer[i] = (num % 2) + '0';
+		num /= 2;
+		i++;
+	}
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = buffer[j];
+		buffer[j] = buffer[i - j - 1];
+		buffer[i - j - 1] = temp;
+	}
+	buffer[i] = '\0';
+	count = i;
+	return (count);
+}
+/**
  * _printf - Print formatted output to stdout
  * @format: The format string
  * @...: Variable number of arguments
@@ -8,8 +44,8 @@
  */
 int _printf(const char *format, ...)
 {
-	int char_count = 0, c, len, num, count;
-	char buffer[4096];
+	int char_count = 0, count, c, len, num;
+	char buffer[12];
 	const char *str;
 	va_list args;
 
@@ -30,32 +66,3 @@ int _printf(const char *format, ...)
 				c = va_arg(args, int);
 				write(1, &c, 1);
 				char_count++;
-			}
-			else if (*format == 's')
-			{
-				str = va_arg(args, const char *);
-				len = 0;
-				while (str[len] != '\0')
-					len++;
-				write(1, str, len);
-				char_count += len;
-			}
-			else if (*format == '%')
-			{
-				write(1, "%", 1);
-				char_count++;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				num = va_arg(args, int);
-				count = snprintf(buffer, sizeof(buffer), "%d", num);
-				write(1, buffer, count);
-				char_count += count;
-			}
-		}
-		format++;
-	}
-
-	va_end(args);
-	return (char_count);
-}
